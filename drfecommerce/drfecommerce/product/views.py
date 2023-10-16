1f345a2fbdcdbed73a68b213d4c22a1561bce0a9
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-10-16 21:19:55 +0330

    update models.py, views.py and urls.py

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+
+class CategoryView(viewsets.ViewSet):
+    """
+    A simple viewset for viewwing data
+    """
+
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
