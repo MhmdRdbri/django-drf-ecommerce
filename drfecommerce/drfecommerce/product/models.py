2131d630b6182c5fdf7865c1b1c89fecd2ab459c
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-10-16 19:19:24 +0330

    update models.py

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from mptt.models import MPTTMpodel, TreeForeignKey
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    parent = Tree
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    is_digital = models.BooleanField(default=False)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
